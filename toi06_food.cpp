23d69491d64b233aa692ba5a8f1cf29ad17a1eb0
Borworntat Dendumrongkul <64594596+MasterIceZ@users.noreply.github.com>
2020-11-14 08:43:42 +0700

    Food

---
+++ toi06_food.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define space " "
+#define int long long
+#define double long double
+#define sim template<typename T
+#define fi first
+#define se second
+
+#define pii pair<int,int>
+#define pb push_back
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(),(x).end()
+#define ub upper_bound
+#define lb lower_bound
+#define dec(_) cout << fixed << setprecision(_)
+#define EACH(a) for(auto x : (a))
+
+sim>void __test(vector<T>v){
+	for(auto x : v){
+		cout << x << " ";
+	}
+	cout << endl;
+}
+sim>vector<T> dp_qs(vector<T>dp,size_t nax){
+	for(int i=1;i<=nax;++i){
+		dp[i] += dp[i-1];
+	}
+	return dp;
+}
+
+int gcd(int a,int b){
+	if(a==0){
+		return b;
+	} 
+	if(b==0){
+		return a;
+	} 
+	return gcd(b%a,b);
+}
+int lcm(int a,int b){
+	return a*b/gcd(a,b);
+}
+string reverse(string s){
+	int n = s.size(),cnt=0; 
+	string rev = s; 
+	for(int i=n-1;i>=0;--i){
+		rev[cnt++] = s[i];
+	}
+   	return rev;
+}
+void writeCase(int _){
+	cout << "Case #" <<  _ << ": \n"; 
+}
+typedef long long ll;
+bool __Q=0;
+bool __writecase=0;
+int n,m;
+const int INF = 2e18;
+
+void solution(){
+	vector<int>key = {1,2,3,4,5,6,7,8};
+	vector<bool>mark(10,false);
+	cin >> n;
+	cin >> m;
+	for(int i=0;i<m;++i){
+		int x;
+		cin >> x;
+		mark[x] = true;
+	}
+	do{
+		if(mark[key[0]] == false){
+			for(int i=0;i<n;++i){
+				cout << food[i] << space ;
+			}
+			cout << endl;
+		}
+	}while(next_permutation(key.begin(),key.begin()+n));
+	return ;
+}
+
+int32_t main (){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int T=1;
+	int _____=0;
+	if(__Q){
+		cin >> T;
+	}
+	do{
+		if(__writecase){
+			writeCase(_____++);
+		}
+		solution();	
+		cout << '\n';
+	}while(--T);
+	return 0;
+}
